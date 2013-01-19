eb615c42ce91819a51b83d3e989191bf26685865
Riley <wikihuntley@gmail.com>
2013-01-19 14:57:59 -0800

    Create localconfig.py

---
+++ localconfig.py
@@ -0,0 +1,14 @@
+##Main Settings
+botname = "RileyBot"
+
+##Default System Path
+winpath = "C:\Python27\pywikipedia"#Windows path for pywikipedia
+linuxpath = "/home/riley/pywikipedia/pywikipedia"
+
+#Onwiki paths
+gopage = "User:RileyBot/Graffiti wall/Run"
+timepage = "User:RileyBot/Graffiti wall/Time"
+
+#Runtime edit summarties
+primarytaskname = "Graffiti wall cleaning"
+editsumtime = "Updating run time) ([[Wikivoyage:Script policy|Bot]])"
