b26b9b7397d2922a28c3c3587e5037c2846e0065
Riley <wikihuntley@gmail.com>
2013-04-02 20:30:39 -0700

    Update task10.py
    hm

--- task10.py
+++ task10.py
@@ -6,6 +6,8 @@ import robot
 creator = page.getCreator() 
 reason = u'Non-notable diplomat stub article. For the relevant notability policy, please see [[Wikipedia:Notability_(people)#Diplomats|Wikipedia:Notability (people)#Diplomats]].'
 warn_template = u'{{subst:Proposed deletion notify|%s|concern=%s}} ~~~~'
+self.stop_page = pywikibot.Page(self.site, 'User:RileyBot/Stop/10')
+log = pywikibot.Page(self.site, 'User:RileyBot/Logs/10')
 ## Regex ##
 regex_skip = """\{\{(Template:)?([Pp]ROD
 |[Pp]roD
@@ -13,20 +15,7 @@ regex_skip = """\{\{(Template:)?([Pp]ROD
 |[Pp]roposal to delete)"""
 regex_skip = regex_skip.replace('\n', '')
 REGEX = re.compile(regex_skip, flags=re.IGNORECASE)
-## Need to figure out how this works ##
-def log(title_1):
-    LOGFILE = 'Task10.log'
-    if os.path.isfile(LOGFILE):
-        f = open(LOGFILE, 'r')
-        old = f.read()
-        f.close()
-    else:
-        old = ''
-    msg = '*[[:%s]]\n' % (title_1)
-    f = open(LOGFILE, 'w')
-    f.write(old+msg)
-    f.close()
-	
+
 class ProdBot(robot.Robot):
 	
 	def run(self):
@@ -37,14 +26,15 @@ class ProdBot(robot.Robot):
 	## Does this even work? ##
 	robot.Robot.self.trial = True
 	robot.Robot.self.trial_max = 20
-	self.stop_page = pywikibot.Page(self.site, 'User:RileyBot/Stop/10')
-	self.startLogging(pywikibot.Page(self.site, 'User:RileyBot/Logs/10'))
 	
 	## Check page; [[User:RileyBot/Stop/10]] ##
 	def check_page(self):
 		text = self.stop_page.get(force=True)
         if text.lower() != 'run':
-            raise Exception("Stop page disabled")	
+        	log_contentError = log.get(get_redirect = True)
+			log_contentError = log_contentError + "\n\n" + "# [[:User:RileyBot/Stop/10]]: '''Error: Stop page disabled.''' {{subst:#time: r|now}}" + "\n"
+			log.put(log_contentError, "[[User:RileyBot|Bot]]: Logging error; Stop page disabled.) ([[User:RileyBot/10|Task 10]]")
+            raise Exception("Stop page disabled")
 	def do_page(self, page):	
         	title_1 = page.title()
 		if page.isRedirectPage():
@@ -70,7 +60,7 @@ class ProdBot(robot.Robot):
                         u'Skipping %s because of edit conflict'
                         % (page.title()))
 	
-	def warn_user(self, page2)
+	def warn_user(self)
 		warn_title = pywikibot.Page(wiki, 'User talk:'+creator)
 		if page.isRedirectPage():
             		self.output('Page %s is a redirect; skipping.' % page.title())
@@ -84,9 +74,12 @@ class ProdBot(robot.Robot):
 		self.check_page()
 		warn_page.edit(warn_text, section="new", sectiontitle="== [[Wikipedia:Proposed deletion|Proposed deletion]] of %s ==", summary="[[User:RileyBot|Bot]] notification: proposed deletion of %s.) ([[User:RileyBot/10|Task 10]]", bot=10) % (warn_title, warn_title)
 		print warn_text
+	def log(self)
+		## See line 10 for the log page ##
+		## We don't check the checkpage here because we want to remember what pages we tagged (even if the bot makes errors) ##
+		log_content = log.get(get_redirect = True)
+		log_content = log_content + "\n" + "# [[:" + title_1 + "]]:" + reason + "{{subst:#time: r|now}}"
+		log.put(log_content, "[[User:RileyBot|Bot]]: Logging [[WP:proposed deletion|proposed deletion]] nomination of [[" + title_1 + "]].) ([[User:RileyBot/10|Task 10]]")
 if __name__ == "__main__":
     bot = ProdRobot()
-    try:
-        bot.run()
-    finally:
-        bot.pushLog()
+    bot.run()
