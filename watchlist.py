3b7e8e58ef7c1327141f54377ca983ba74b7e5dc
Riley <wikihuntley@gmail.com>
2013-04-07 00:53:35 -0700

    Update watchlist.py

--- watchlist.py
+++ watchlist.py
@@ -1,21 +1,95 @@
 import mwclient
+from time import mktime
+from datetime import datetime
+import time
+import codecs
 import settings
-site = mwclient.Site(settings.url)
-site.login(settings.username, settings.password)
+# CC-BY-SA Theopolisme
+# page where run logs are uploaded to
+site2 = mwclient.Site('en.wikipedia.org')
+# page where watchlist is checked
+log_page = site2.Pages['User:RileyBot/Logs/Watchlist']
+l = []
+def log(text):
+  # timestamp every item to make tracking easier, also use UTC time to avoid local issues
+  tm = time.strftime(u'%Y-%m-%d %H:%M:%S',time.gmtime())
+  f3 = codecs.open('RileyBotWatchlist.txt', 'a', 'utf-8')
+  # post every item to its own line
+  f3.write('\n* %s\t%s' % (tm,text))
+  f3.close()
+  
+def check_page():
+  # get check page and if contents are not 'enable' have the bot log that the bot has been disabled
+  text = page3.get
+  if text.lower() != u'enable':
+    log('Check page disabled')
+    
+def generate(wiki):
+    print "Working on " + wiki
+	log('Working on' + wiki)
+	site1 = mwclient.Site(wiki)
+	site1.login(settings.username, settings.password)
+	data = site1.watchlist(prop="ids|timestamp|title")
+	page3 = site1.Pages['User:RileyBot/Stop']
+	check_page()
+	print data
+	
+	global l
+	l.append("\n=== " + wiki + " ===")
 
-def get_info(**kwargs):
-    call = {'action':'query',
-            'list':'watchlist',
-                'wllimit':'5',
-                'wlowner':settings.username,
-                'wltoken':settings.token,
-                'wlshow':'!bot',
-                'wlprop':'user|comment|timestamp|sizes|flags',
-                'format':'xml'
-    }
-data = site.api(params)
-print data
-if not 'wlstart' in data['query-continue']['watchlist']:
-    print('You have no items in your watchlist or none of your watched items were edited in the time period displayed.!')
-else:
-    print('You have items in your watchlist and your watched items were edited in the time period displayed, moving on...')
+	l.append("""\n{| class="wikitable sortable"
+	|-
+	! Page title !! Revision timestamp !! Diff""")
+
+	iz = 0
+	for x in data:
+		if iz <= 25:
+			dt = datetime.fromtimestamp(mktime(x['timestamp']))
+			l.append("\n|-\n| " + """<span class="plainlinks">[//""" + wiki + "/wiki/" + x['title'].replace(" ","_") + " " + x['title'] + "]</span> || " + str(dt) + " || " + "[//" + wiki + "/w/index.php?diff=prev&oldid=" + str(x['revid']) + " diff]")
+			iz = iz + 1
+		else:
+			print "That's all for now!"
+
+	l.append("\n|}")
+
+# @Riley_Huntley - Edit this list of wikis to suit your liking.
+wikis = ['en.wikipedia.org',
+'en.wikiquote.org',
+'en.wikivoyage.org',
+'es.wikivoyage.org',
+'fr.wikivoyage.org',
+'he.wikisource.org', 
+'simple.wikipedia.org', 
+'sa.wikipedia.org',
+'wikisource.org', 
+'sv.wikivoyage.org',
+'test2.wikipedia.org', 
+'test.wikipedia.org', 
+'tt.wiktionary.org']
+
+for wiki in wikis:
+	generate(wiki)
+
+final = ''.join(l)
+	
+# @Riley_Huntley - You can change this to log in as your own bot if you'd like.
+site2.login(settings.username, settings.password)
+
+page = site2.Pages['User:RileyBot/watchlist']
+page.save(final, summary = "[[User:RileyBot|Bot]]: Updating global watchlist")
+log('Global watchlist updated')
+def shut_down():
+  # log shutdown
+  log(u'Run Ended')
+  # get the contents of the log file
+  f = codecs.open('RileyBotWatchlist.txt','r', 'utf-8')
+  log_text = f.read()
+  f.close()
+  text = log_page.get()
+  # post the log file to the wiki log page
+  log_page.put(log_text,'[[User:RileyBot|Bot]]: Uploading logs for [[User:RileyBot/Watchlist|Watchlist]]')
+  sys.exit(0)
+# general logging function
+
+if __name__ == "__main__":
+  main()
