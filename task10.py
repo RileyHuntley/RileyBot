8a6ca89a200a50539e752b44b1d29e318933aa39
Riley <wikihuntley@gmail.com>
2013-04-07 02:02:34 -0700

    Update task10.py

--- task10.py
+++ task10.py
@@ -1,128 +1,82 @@
-#!/usr/bin/python
-# -*- coding: utf-8  -*-
-import sys 
-import pagegenerators
-import codecs
-import os
-import re
+import mwclient
+from time import mktime
+from datetime import datetime
 import time
-import wikipedia
-# array of strings to search for and exclude those from re-tagging
-regex_skip    =  [
-                  u'prod',
-                  u'proposal to delete',
-                  u'proposed deletion',
-                 ]
-regex_skip    = ur'\{(Template:)?('+u'|'.join(regex_skip)+u')'
-REGEX         = re.compile(regex_skip, flags=re.IGNORECASE)
-site          = wikipedia.getSite()
+import codecs
+import settings
+# CC-BY-SA Theopolisme
 # page where run logs are uploaded to
-log_page      = wikipedia.Page(site,u'User:RileyBot/Logs/10')
-# links from this page are what get tagged
-source_page   = wikipedia.Page(site,u'User:RileyBot/Trial/10')
-# automated shutdown feature
-stop_page     = wikipedia.Page(site,u'User:RileyBot/Stop/10')
-# PROD reason
-tag_reason    = u'Non-notable diplomat stub article. For the relevant notability policy, please see [[Wikipedia:Notability_(people)#Diplomats|Wikipedia:Notability (people)#Diplomats]].'
-# user talk page notice
-warn_template = u'{{subst:Proposed deletion notify|%s|concern='+tag_reason+'}} ~~~~'
-
-def main():
-  log('Run started')
-  # get a list of pages to tag
-  pages_to_work_on = [x for x in pagegenerators.NamespaceFilterPageGenerator(source_page.linkedPages(),[0])]
-  for page in pages_to_work_on:
-    # is the bot allowed to do this task?
-    check_page()
-    # if page has already been deleted, log and skip
-    if page.exists():
-      # check if it is a redirect, if it is log and skip
-      if not page.isRedirectPage():
-        # get page contents
-        text = page.get()
-        # check to see if there is a template on the black list, if it is log and skip
-        if not REGEX.search(text):
-          # add PROD notice
-          newtext = '{{subst:Proposed deletion|%s}}\n%s' % (tag_reason,text)
-          try:
-            # try and save the page and log it
-            page.put(newtext, comment=u'[[User:RileyBot|Bot]] trial: Nominating [[%s]] for [[WP:proposed deletion|proposed deletion]] by request of [[User:Kleinzach|Kleinzach]].) ([[User:RileyBot/10|Task 10]]' % page.title(), watchArticle = False, minorEdit = True)
-            log(u'Tagging [[%s]]' % page.title())
-          # page is protected
-          except wikipedia.LockedPage:
-            log(u"Page %s is locked; skipping." % page.title())
-          # Edit conflicted with another user
-          except wikipedia.EditConflict:
-            log(u'Skipping %s because of edit conflict' % (page.title()))
-          # some unknown error has occurred, log and review at a latter date
-          except:
-            log(u'Skipping %s because of unknown error' % (page.title()))
-          # page has been tagged for PROD now need to notify the creator
-          warn_user(page)
-        else:
-          log('[[%s]] ignored due to regular expression' % page.title())
-      else:
-        log('Page %s is a redirect; skipping.' % page.title())
-    else:
-      log('Page %s does not exist; skipping.' % page.title())
-  shut_down()
-  
-def check_page():
-  # get check page and if contents are not 'enable' have the bot shutdown as someone has disabled the bot
-  text = stop_page.get(force=True)
-  if text.lower() != u'enable':
-    log('Check page disabled')
-    shut_down()
-# notfy a user about a page that has been tagged for deletion, dont check the stop page as we want to avoid not notifying the creator of the talk page
-def warn_user(page):
-  # get the first person to edit the page not 100% reliable but is the best for identifying who created a page
-  creator,ts = page.getCreator()
-  # create a page object for their talk page
-  talk_page = wikipedia.Page(site,u'User talk:%s' % creator)
-  # due to renames, switching accounts and what not sometimes the talk page has been redirected to a different username if so find out and use that as the correct page
-  if talk_page.isRedirectPage():
-    talk_page = talk_page.getRedirectTarget()
-    if not talk_page.isTalkPage():
-      # for some reason the new talk page isnt a talk page (banned/blocked user, whos talk page redirects to their user page ect)
-      log(u'Unable to locate talk page for [[User:%s]]' % creator)
-      return
-  # format talk page notice
-  warn_text = warn_template % (page.title())
-  # get user talk page contents
-  text = talk_page.get()
-  # combine the two
-  text+=u"\n%s" % warn_text
-  try:
-    # save the talk page and log the details
-    talk_page.put(text, u"[[User:RileyBot|Bot]] notification: Proposed deletion of [[%s]].) ([[User:RileyBot/10|Task 10]]" % page.title())
-    log(u'Notifying [[User:%s]] about [[%s]]' % (creator,page.title()))
-  except wikipedia.LockedPage:
-    log(u"Page %s is locked; skipping." % (talk_page.title()))
-  except wikipedia.EditConflict:
-    log(u'Skipping %s because of edit conflict' % (talk_page.title()))
-  except:
-    log(u'Skipping %s because of unknown error' % (talk_page.title()))
-  return
-# used to wrap up and cleanly exit the program, can be called from mutiple locations within the program 
-def shut_down():
-  # log shutdown
-  log(u'Run Ended')
-  # get the contents of the log file
-  f = codecs.open('RileyBot10.txt','r', 'utf-8')
-  log_text = f.read()
-  f.close()
-  text = log_page.get()
-  # post the log file to the wiki log page
-  log_page.put(log_text,'Uploading logs for [[User:RileyBot/10|Task 10]]')
-  sys.exit(0)
-# general logging function
+site2 = mwclient.Site('en.wikipedia.org')
+# page where watchlist is checked
+log_page = site2.Pages['User:RileyBot/Logs/Watchlist']
+l = []
 def log(text):
-  # timestamp every item to make tracking easier, also use UTC time to avoid local issues
-  tm = time.strftime(u'%Y-%m-%d %H:%M:%S',time.gmtime())
-  f3 = codecs.open('RileyBot10.txt', 'a', 'utf-8')
-  # post every item to its own line
-  f3.write('\n* %s\t%s' % (tm,text))
-  f3.close()
+        # timestamp every item to make tracking easier, also use UTC time to avoid local issues
+        tm = time.strftime(u'%Y-%m-%d %H:%M:%S',time.gmtime())
+        f3 = codecs.open('RileyBotWatchlist.txt', 'a', 'utf-8')
+        # post every item to its own line
+        f3.write('\n* %s\t%s' % (tm,text))
+        f3.close()
 
-if __name__ == "__main__":
-  main()
+def generate(wiki):
+        print "Working on " + wiki
+        log('Working on' + wiki)
+        site1 = mwclient.Site(wiki)
+        site1.login(settings.username, settings.password)
+        data = site1.watchlist(prop="ids|timestamp|title")
+        print data
+
+        global l
+        l.append("\n=== " + wiki + " ===")
+
+        l.append("""\n{| class="wikitable sortable"
+        |-
+        ! Page title !! Revision timestamp !! Diff""")
+
+        iz = 0
+        for x in data:
+                if iz <= 25:
+                        dt = datetime.fromtimestamp(mktime(x['timestamp']))
+                        l.append("\n|-\n| " + """<span class="plainlinks">[//""" + wiki + "/wiki/" + x['title'].replace(" ","_") + " " + x['title'] + "]</span> || " + str(dt) + " || " + "[//" + wiki + "/w/index.php?diff=prev&oldid=" + str(x['revid']) + " diff]")
+                        iz = iz + 1
+                else:
+                        print "That's all for now!"
+
+        l.append("\n|}")
+
+wikis = ['en.wikipedia.org',
+'en.wikiquote.org',
+'en.wikivoyage.org',
+'es.wikivoyage.org',
+'fr.wikivoyage.org',
+'he.wikisource.org', 
+'simple.wikipedia.org', 
+'sa.wikipedia.org',
+'wikisource.org', 
+'sv.wikivoyage.org',
+'test2.wikipedia.org', 
+'test.wikipedia.org', 
+'tt.wiktionary.org']
+
+for wiki in wikis:
+        generate(wiki)
+
+final = ''.join(l)
+
+site2.login(settings.username, settings.password)
+
+page = site2.Pages['User:RileyBot/watchlist']
+page.save(summary = "[[User:RileyBot|Bot]]: Updating global watchlist")
+log('Global watchlist updated')
+def shut_down():
+        # log shutdown
+        log(u'Run Ended')
+        # get the contents of the log file
+        f = codecs.open('RileyBotWatchlist.txt','r', 'utf-8')
+        log_text = f.read()
+        f.close()
+        text = log_page.get()
+        # post the log file to the wiki log page
+        log_page.save(log_text,'[[User:RileyBot|Bot]]: Uploading logs for [[User:RileyBot/Watchlist|Watchlist]]')
+        sys.exit(0)
+        # general logging function
