4dbb40e7d9fe8a1ba045b7ebf5445511e17b9135
Riley <wikihuntley@gmail.com>
2013-04-06 20:43:40 -0700

    Update watchlist.py
    hm

--- watchlist.py
+++ watchlist.py
@@ -2,19 +2,18 @@ import mwclient
 import settings
 site = mwclient.Site(settings.url)
 site.login(settings.username, settings.password)
-## https://en.wikipedia.org/w/api.php?action=query&list=watchlist&wllimit=500&wlowner=Riley%20Huntley&wltoken=479782&wlshow=!bot&wlprop=user|comment|timestamp|sizes|flags&format=jsonfm
-kwargs = {'action':'query',
-          'list':'watchlist',
-                  'wllimit':'5',
-  	  	  'wlowner':settings.username,
-		  'wltoken':settings.token,
-		  'wlshow':'!bot',
-		  'wlprop':'user|comment|timestamp|sizes|flags',
-		  'format':'jsonfm'
-}
 
-
-data = site.api(**kwargs)
+def get_info(**kwargs):
+    call = {'action':'query',
+            'list':'watchlist',
+                'wllimit':'5',
+                'wlowner':settings.username,
+                'wltoken':settings.token,
+                'wlshow':'!bot',
+                'wlprop':'user|comment|timestamp|sizes|flags',
+                'format':'xml'
+    }
+data = site.api(params)
 print data
 if not 'wlstart' in data['query-continue']['watchlist']:
     print('You have no items in your watchlist or none of your watched items were edited in the time period displayed.!')
