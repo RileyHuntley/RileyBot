de964a8cf6af7629a8f682e3c5b40657d33c657b
theopolisme <theopolismewiki@gmail.com>
2013-04-18 17:38:39 -0500

    A variety of fixes
    * lowercase variable names
    * add list 'skip_these' for pages that should be skipped
    * fix completely broken tagging mechanism
    * better error handling
    * more logging, you can of course disable if you're so inclined.

--- talkpage.py
+++ talkpage.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python
-# -*- coding: utf-8  -*-
+# -*- coding: utf-8	 -*-
 import sys
 import catlib
 import pagegenerators
@@ -8,49 +8,56 @@ import codecs
 import os
 import re
 import time
-regex_skip    =  [
-                  u'extant organization',
-                  u'COI editnotice',
-                 ]
-regex_skip    = ur'\{(Template:)?('+u'|'.join(regex_skip)+u')'
-REGEX         = re.compile(regex_skip, flags=re.IGNORECASE)
+
+regex_skip	  =	 [u'extant organization', u'COI editnotice']
+regex_skip	  = ur'\{(Template:)?('+u'|'.join(regex_skip)+u')'
+regexp		   = re.compile(regex_skip, flags=re.IGNORECASE)
+skip_these = [u'Organization']
+
 site = wikipedia.getSite()
-log_page      = wikipedia.Page(site,u'User:RileyBot/Logs/11')
-stop_page     = wikipedia.Page(site,u'User:RileyBot/Stop/11')
+log_page	  = wikipedia.Page(site,u'User:RileyBot/Logs/11')
+stop_page	  = wikipedia.Page(site,u'User:RileyBot/Stop/11')
 def main():
-    log('Run started')
-    cat = catlib.Category(site,'Category:Organizations')
-    gen = pagegenerators.CategorizedPageGenerator(cat)
-    for page in gen:
-        check_page()
-        if page.exists():
-            if not page.isRedirectPage():
-                page = "Talk:%s" % (page)
-                text = page.get()
-                if not REGEX.search(text):
-                    newtext = '{{COI editnotice}}\n%s' % (text)
-                    try:
-                        page.put(newtext, comment=u'[[User:RileyBot|Bot]] trial: Adding [[Template:COI editnotice]] to [[%s]].) ([[User:RileyBot/11|Task 11]]' % page.title(), watchArticle = False, minorEdit = True)
-                        log(u'Saved edit on [[%s]]' % page.title())
-                    except wikipedia.LockedPage:
-                        log(u"Page %s is locked; skipping." % page.title())
-                    except wikipedia.EditConflict:
-                        log(u'Skipping %s because of edit conflict' % (page.title()))
-                    except:
-                        log(u'Skipping %s because of unknown error' % (page.title()))
-                else:
-                    log('[[%s]] ignored due to regular expression' % page.title())
-            else:
-                log('Page %s is a redirect; skipping.' % page.title())
-        else:
-            log('Page %s does not exist; skipping.' % page.title())
-    shut_down()
+	log('Run started')
+	cat = catlib.Category(site,'Category:Organizations')
+	gen = pagegenerators.CategorizedPageGenerator(cat)
+	for page in gen:
+		if page.title() not in skip_these:
+			check_page()
+			if page.exists():
+				if not page.isRedirectPage():
+					page2 = "Talk:%s" % (page.title())
+					page2 = wikipedia.Page(site,page2)
+					try:
+						text = page2.get()
+					except wikipedia.NoPage:
+						text = u''
+					if not regexp.search(text):
+						newtext = '{{COI editnotice}}\n%s' % (text)
+						try:
+							page2.put(newtext, comment=u'[[User:RileyBot|Bot]] trial: Adding [[Template:COI editnotice]] to [[%s]].) ([[User:RileyBot/11|Task 11]]' % page.title(), watchArticle = False, minorEdit = True)
+							log(u'Saved edit on [[%s]]' % page2.title())
+						except wikipedia.LockedPage:
+							log(u"Page %s is locked; skipping." % page2.title())
+						except wikipedia.EditConflict:
+							log(u'Skipping %s because of edit conflict' % (page2.title()))
+						except:
+							log(u'Skipping %s because of unknown error' % (page2.title()))
+					else:
+						log('[[%s]] ignored due to regular expression' % page2.title())
+				else:
+					log('Page %s is a redirect; skipping.' % page2.title())
+			else:
+				log('Page %s does not exist; skipping.' % page2.title())
+		else:
+			log('Page %s was in skip list.' % page2.title())
+	shut_down()
 def check_page():
   text = stop_page.get(force=True)
   if text.lower() != u'enable':
-    log('Check page disabled')
-    shut_down()
-    
+	log('Check page disabled')
+	shut_down()
+	
 def shut_down():
   log(u'Run Ended')
   f = codecs.open('RileyBot11.txt','r', 'utf-8')
@@ -59,8 +66,10 @@ def shut_down():
   text = log_page.get()
   log_page.put(log_text,'Uploading logs for [[User:RileyBot/11|Task 11]]')
   sys.exit(0)
+
 def log(text):
   tm = time.strftime(u'%Y-%m-%d %H:%M:%S',time.gmtime())
+  print tm + ": " + text
   f3 = codecs.open('RileyBot11.txt', 'a', 'utf-8')
   f3.write('\n* %s\t%s' % (tm,text))
   f3.close()
