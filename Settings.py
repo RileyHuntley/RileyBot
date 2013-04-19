c893708aa00ad18d22c1b09befbe048ede8a9f65
Riley Huntley <wikihuntley@gmail.com>
2013-04-19 06:14:09 -0700

    Update
    Update

--- Settings.py
+++
@@ -1,8 +0,0 @@
-database = '/home/path/to/snitch.db'
-
-snatch_network = 'irc.wikimedia.org'
-snitch_network = 'irc.freenode.net'
-
-nickserv_password = '...'
-
-authorized_users = ('wikimedia/Riley-Huntley', 'wikipedia/...')
